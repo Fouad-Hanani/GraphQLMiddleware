b27b53f4f7eab0b08b38db2c45f9391d83f004fb
Fouad-Hanani <info.fouadhanani@gmail.com>
2020-12-08 13:16:53 +0100

    return the value of fn(...)

--- GraphQLMiddleware.js
+++ GraphQLMiddleware.js
@@ -4,7 +4,7 @@
 module.exports = (validationFn, errorMessage, fn) => {
 	return (parent, args, context, params) => {
 		if (validationFn(context)) {
-			fn(parent, args, context, params);
+			return fn(parent, args, context, params);
 		} else {
 			throw new Error(errorMessage);
 		}
